3bdc7bc643f698fbcadea26a0b230ce25023ab89
FadyAttia <fadyattia11@gmail.com>
2020-10-08 13:44:37 +0200

    added firebase config file & connected to firebase database (#3)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Navbar from './components/Home/Navbar/Navbar'
 import Footer from './components/Footer/Footer'
 import Routes from './components/Routes/Routes'
 
+import './firebase/firebase'
+
 const App = () => {
   return (
     <Router>
